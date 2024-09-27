2778ac78b2fd286923d1582d302ae17e65530186
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2024-09-27 19:52:50 +0300

    Added one method to the dog class

--- Dog.java
+++ Dog.java
@@ -2,6 +2,9 @@ public class Dog  extends Animal {
    public Dog(String name) {
 
    }
+   public void eat(){
+      System.out.println("Dog eat");
+   }
 
    public static void main(String[] args) {
       Dog d = new Dog("Dog");
